f22819eda1218189bdc331d2d24e258cd62e2ad0
arturoaram <arturoaram0@gmail.com>
2016-02-27 20:56:51 -0500

    And added my example for the working ParseQuery!
    Changing the Manifest to be into my FlashCards Project in Parse, not jadeFlash

--- app/src/main/java/com/examples/owner/flashcards/LoginActivity.java
+++ app/src/main/java/com/examples/owner/flashcards/LoginActivity.java
@@ -2,17 +2,9 @@ package com.examples.owner.flashcards;
 
 import android.app.Activity;
 import android.content.Intent;
-
 import android.os.Bundle;
-
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-
-
-
-
-
 
 public class LoginActivity extends Activity {
         EditText username,password;
